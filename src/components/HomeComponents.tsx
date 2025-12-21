c7bb8c254806b52f3a2898fb56027716a7b077c4
ahmadfauzaan613 <ahmadfauzaan613@gmail.com>
2025-12-21 23:12:32 +0700

    refactor: reorganize imports and update developer title in HomeComponents

--- src/components/HomeComponents.tsx
+++ src/components/HomeComponents.tsx
@@ -1,13 +1,13 @@
 'use client'
-import React from 'react'
-import { motion } from 'framer-motion'
-import Image from 'next/image'
-import { result } from 'lodash'
-import Linkdata from '@/data/HomeData.json'
-import { IDataLinkType, ButtonFunctionProps } from '@/types/HomeType'
-import Link from 'next/link'
 import { Button } from '@/components/ui/button'
+import Linkdata from '@/data/HomeData.json'
+import { ButtonFunctionProps, IDataLinkType } from '@/types/HomeType'
+import { motion } from 'framer-motion'
+import { result } from 'lodash'
 import { File, FileUser, Linkedin, Mail, Phone } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
 export default function HomeComponents() {
   const ButtonFunction: React.FC<ButtonFunctionProps> = ({ data }) => {
@@ -27,17 +27,47 @@ export default function HomeComponents() {
     }
   }
   return (
-    <motion.div className="mt-10 px-4 sm:px-6 lg:px-8" initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
-      <motion.h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold" initial={{ opacity: 0, x: -50 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 0.2, duration: 0.8 }}>
+    <motion.div
+      className="mt-10 px-4 sm:px-6 lg:px-8"
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+    >
+      <motion.h1
+        className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold"
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.2, duration: 0.8 }}
+      >
         AHMAD FAUZAN
       </motion.h1>
-      <motion.h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold uppercase" initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 0.4, duration: 0.8 }}>
-        Full stack developer
+      <motion.h1
+        className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold uppercase"
+        initial={{ opacity: 0, x: 50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.4, duration: 0.8 }}
+      >
+        Front End Developer
       </motion.h1>
-      <motion.div className="w-full bg-black h-[30vh] sm:h-[40vh] md:h-[50vh] mt-10 relative" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.6, duration: 1 }}>
-        <Image src="/images/Bersoreria12814-BW.jpg" alt="Gambar dari Pinterest" fill className="object-cover" />
+      <motion.div
+        className="w-full bg-black h-[30vh] sm:h-[40vh] md:h-[50vh] mt-10 relative"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.6, duration: 1 }}
+      >
+        <Image
+          src="/images/Bersoreria12814-BW.jpg"
+          alt="Gambar dari Pinterest"
+          fill
+          className="object-cover"
+        />
       </motion.div>
-      <motion.div className="flex justify-end flex-wrap gap-4 mt-8" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.8, duration: 1 }}>
+      <motion.div
+        className="flex justify-end flex-wrap gap-4 mt-8"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.8, duration: 1 }}
+      >
         {result(Linkdata, 'Linkdata.dataLink', []).map((item: IDataLinkType, id: number) => (
           <Link href={item.url} target="_blank" rel="noopener noreferrer" key={id}>
             <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
