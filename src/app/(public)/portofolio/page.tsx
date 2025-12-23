de2cdc960f9ba62faed9e4bdc7695412bedde97a
ahmadfauzaan613 <ahmadfauzaan613@gmail.com>
2025-12-24 02:36:42 +0700

    feat: enable dynamic rendering for portfolio page

--- src/app/(public)/portofolio/page.tsx
+++ src/app/(public)/portofolio/page.tsx
@@ -1,6 +1,8 @@
 import PortoComponents from '@/components/PortoComponents'
 import { ProjectRepository } from '../../repository/project.repository'
 
+export const dynamic = 'force-dynamic'
+
 export default async function pages() {
   const portfolios = await ProjectRepository.findAll()
   return <PortoComponents portfolios={portfolios} />
